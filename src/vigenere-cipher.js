f95d3c93749a943b3418e89c6d780aa95d69eb76
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-04-29 00:22:33 +0300

    feat: implement encrypt to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+	constructor(bool) {
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.machine = bool;
+    this.message = "";
+    this.key = "";
+    this.squareVigenere = [];
+    this.newMessage = "";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+	generateSquareVigener() {
+    let alphabetArr = this.alphabet.split("");
+    for (let i = 0; i < this.alphabet.length; i++) {
+      this.squareVigenere.push([
+        ...alphabetArr.slice(i),
+        ...alphabetArr.slice(0, i)
+      ]);
+    }
+  }
+	isCorrectArgs() {
+    if (!!this.message && !!this.key) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  transformArgs() {
+    this.message = this.message.toUpperCase();
+    let repeatCountKey = Math.ceil(this.message.length / this.key.length);
+    this.key = this.key
+      .toUpperCase()
+      .repeat(repeatCountKey)
+      .slice(0, this.message.length);
+  }
+  encrypt(message, key) {
+    this.message = message;
+    this.key = key;
+    if (this.isCorrectArgs()) {
+      this.transformArgs();
+      this.generateSquareVigener();
+      let offsetY = 0;
+      for (let i = 0; i < this.message.length; i++) {
+        let indX = this.alphabet.indexOf(this.message[i]);
+        if (indX !== -1) {
+          let indY = this.alphabet.indexOf(this.key[i - offsetY]);
+          this.newMessage += this.squareVigenere[indX][indY];
+        } else {
+          offsetY++;
+          this.newMessage += this.message[i];
+        }
+      }
+			const encrypted = this.newMessage;
+			this.newMessage = '';
+      return encrypted;
+    } else {
+      throw new Error("Incorrect arguments!");
+    }
+  }
+  decrypt(message, key) {
+		this.message = message;
+    this.key = key;
+    if (this.isCorrectArgs()) {
+      console.log("Correct argements");
+
+			// TODO: implement decrypt
+
+    } else {
+      throw new Error("Incorrect arguments!");
+    }
   }
 }
 
